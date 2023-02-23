9723ec7efb93db4bc2d7e6ad583ff1327fb3e107
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-02-23 13:59:54 +0200

    Actually ACTUALLY fix version clobber

--- src/helix-editor/install.sh
+++ src/helix-editor/install.sh
@@ -54,13 +54,13 @@ case "${ADJUSTED_ID}" in
 esac
 
 cd /tmp
-curl -fsSLO https://github.com/helix-editor/helix/releases/download/${VERSION}/helix-${VERSION}-x86_64-linux.tar.xz
-tar xJf helix-${VERSION}-x86_64-linux.tar.xz
-rm -f helix-${VERSION}-x86_64-linux.tar.xz
+curl -fsSLO https://github.com/helix-editor/helix/releases/download/${HELIX_VERSION}/helix-${HELIX_VERSION}-x86_64-linux.tar.xz
+tar xJf helix-${HELIX_VERSION}-x86_64-linux.tar.xz
+rm -f helix-${HELIX_VERSION}-x86_64-linux.tar.xz
 mkdir -p  ${_CONTAINER_USER_HOME}/.local/bin
-mv helix-${VERSION}-x86_64-linux/hx ${_CONTAINER_USER_HOME}/.local/bin
+mv helix-${HELIX_VERSION}-x86_64-linux/hx ${_CONTAINER_USER_HOME}/.local/bin
 mkdir -p  ${_CONTAINER_USER_HOME}/.config/helix
-mv helix-${VERSION}-x86_64-linux/runtime ${_CONTAINER_USER_HOME}/.config/helix
-rm -rf helix-${VERSION}-x86_64-linux
+mv helix-${HELIX_VERSION}-x86_64-linux/runtime ${_CONTAINER_USER_HOME}/.config/helix
+rm -rf helix-${HELIX_VERSION}-x86_64-linux
 
-echo "Helix ${VERSION} installed as ${_CONTAINER_USER_HOME}/.local/bin/hx"
+echo "Helix ${HELIX_VERSION} installed as ${_CONTAINER_USER_HOME}/.local/bin/hx"
