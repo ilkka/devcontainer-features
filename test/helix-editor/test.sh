f005200de6aca3b91fb200afab5721861e005ae9
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-02-23 14:25:29 +0200

    Discover there's tests; try to fix them

--- test/helix-editor/test.sh
+++ test/helix-editor/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This test file will be executed against an auto-generated devcontainer.json that
-# includes the 'hello' Feature with no options.
+# includes the 'helix-editor' Feature with no options.
 #
 # For more information, see: https://github.com/devcontainers/cli/blob/main/docs/features/test.md
 #
@@ -9,26 +9,25 @@
 # {
 #    "image": "<..some-base-image...>",
 #    "features": {
-#      "hello": {}
+#      "helix-editor": {}
 #    },
 #    "remoteUser": "root"
 # }
 #
-# Thus, the value of all options will fall back to the default value in 
-# the Feature's 'devcontainer-feature.json'.
-# For the 'hello' feature, that means the default favorite greeting is 'hey'.
+# Thus, the value of all options will fall back to the default value in the
+# Feature's 'devcontainer-feature.json'.
 #
 # These scripts are run as 'root' by default. Although that can be changed
 # with the '--remote-user' flag.
 # 
 # This test can be run with the following command:
 #
-#    devcontainer features test \ 
-#                   --features hello   \
-#                   --remote-user root \
-#                   --skip-scenarios   \
-#                   --base-image mcr.microsoft.com/devcontainers/base:ubuntu \
-#                   /path/to/this/repo
+#    devcontainer features test    \ 
+#               --features helix-editor   \
+#               --remote-user root \
+#               --skip-scenarios   \
+#               --base-image mcr.microsoft.com/devcontainers/base:ubuntu \
+#               /path/to/this/repo
 
 set -e
 
@@ -38,8 +37,9 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "execute command" bash -c "hello | grep 'hey, $(whoami)!'"
 
-# Report results
+check "validate favorite color" hx --version | grep '22.12'
+
+# Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
 reportResults
